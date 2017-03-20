5706bb973eb8293e63b500793fbd387f47414b3f
Archer <minhang.wang@icloud.com>
2017-03-20 16:29:56 +0800

    demo
    photokit相册

---
+++ photoKitDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  photoKitDemo
+//
+//  Created by Archer on 2017/2/11.
+//  Copyright © 2017年 minhangwang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
